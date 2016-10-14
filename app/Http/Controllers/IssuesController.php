fc2d634f854db40d0cddfca7e63ebc298230d8fc
Aung Khant <theaungkhant@gmail.com>
2016-10-14 22:17:06 +0630

    Backend for storing data from issue form

--- app/Http/Controllers/IssuesController.php
+++ app/Http/Controllers/IssuesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Issue;
+use App\User;
 use App\Http\Requests;
 use App\Category;
 
@@ -13,5 +15,30 @@ class IssuesController extends Controller
     {
     	$categories = Category::all();
     	return view('issues.create', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'title' => 'required',
+    		'category' => 'required',
+    		'priority' => 'required',
+    		'message' => 'required'
+    	]);
+
+    	$issue = new Issue([
+    		'title' => $request->input('title'),
+    		'user_id' => Auth::user()->id,
+    		'issue_id' => strtoupper(str_random(10)),
+    		'category_id' => $request->input('category'),
+    		'priority' => $request->input('priority'),
+    		'message' => $request->input('message'),
+    		'status' => "Open",
+    	]);
+
+    	$issue->save();
+    //	$mailer->sendIssueInformation(Auth::user(), $issue);
+
+    	return redirect()->back()->with("status","Issue has been added");
     }
 }
