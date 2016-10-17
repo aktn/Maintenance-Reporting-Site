07eeee3ba9f1806fcacab06b268c816ce3de11bc
Aung Khant <theaungkhant@gmail.com>
2016-10-17 21:07:44 +0630

    Sending user reported issues to admin by mail

---
+++ app/mailers/AppMailer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mailers;
+
+use App\Ticket;
+use Illuminate\Contracts\Mail\Mailer;
+
+class AppMailer{
+	protected $mailer;
+	protected $fromAddress = 'theaungkhant@gmail.com'; //app email
+	protected $fromName = 'Maintenance Report';
+	protected $to;
+	protected $subject;
+	protected $view;
+	protected $data = [];
+
+	public function _construct(Mailer $mailer)
+	{
+		$this->mailer = $mailer;
+	}
+
+	public function sendIssueInformation($user, Issue, $issue)
+	{
+		$this->to = 'admin@admin.com'; // admin email here
+		$this->subject = "[Issue ID: $issue->issue_id] $issue->title";
+		$this->view = 'emails.issue_info';
+		$this->data = compact('user','issue');
+
+		return $this->deliver();
+	}
+
+	public function deliver()
+	{
+		$this->mailer->send($this->view, $this->data, function($message){
+			$message->from($this->fromAddress, $this->fromName)
+					->to($this->to)->subject($this->subject);
+		});
+	}
+}
---
+++ resources/views/emails/issue_info.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE Html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Maintenance Report Information</title>
+	</head>
+	<body>
+		<p>
+			 {{ ucfirst($user->name) }} has reported an issue.
+		</p>
+		<p>ID: {{ $issue->issue_id }}</p>
+		<p>Title: {{ $issue->title }}</p>
+		<p>Priority: {{ $issue->priority }}</p>
+		<p>Status: {{ $issue->status }}</p>
+	</body>
+</html>
