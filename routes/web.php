db7f2a7584231b75d2a7c44ad2ba3469f22a8ff3
Aung Khant <theaungkhant@gmail.com>
2016-10-18 20:22:38 +0630

    Adding Routes For Login & Registratoin

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,17 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('new_issue','IssuesController@create');
 Route::post('new_issue','IssuesController@store');
+
+
+//User Authentication Routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+//User Registration Routes
+Route::get('auth/register', 'Auth\AuthController@getReigster');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::controllers([
+	'password' => 'Auth\PasswordController',
+]);
