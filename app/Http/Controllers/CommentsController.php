fef20231ea972825405b6769e3f29bd7ccf5e083
Aung Khant <theaungkhant@gmail.com>
2016-10-20 21:59:48 +0630

    Adding controller for comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,5 +8,24 @@ use App\Http\Requests;
 
 class CommentsController extends Controller
 {
-    //
+	public function postComment(Request $request, AppMailer $mailer)
+	{
+		$this->validate($request,[
+			'comment' => 'required'
+		]);
+
+		$comment = Comment::create([
+			'issue_id' => $request->input('issue_id'),
+			'user_id'  => Auth::user()->id,
+			'commnet'  => $request->input('comment');
+		]);
+
+		if($comment->issue->user->id !== Auth::user()->id)
+		{
+			$mailer->sendTicketComments($comment->issue->user, Auth::user(), $comment->issue, $comment);
+		}
+
+		return redirect()->back()->with("status","Submitted!");
+	}    
+
 }
